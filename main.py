46cfb7d3a1335e92c902331d5500e2def01cc660
frankdelan <frank.danil704@gmail.com>
2024-07-30 14:55:18 +0300

    init

---
+++ main.py
@@ -0,0 +1,16 @@
+def second_max(numbers: list[int]) -> tuple[int, int]:
+    if numbers[0] > numbers[1]:
+        first, second = numbers[0], numbers[1]
+    else:
+        first, second = numbers[1], numbers[0]
+
+    for item in numbers:
+        if item > first:
+            second = first
+            first = item
+        elif first > item > second:
+            second = item
+    return first, second
+
+
+print(second_max([1, 2, 5, 54, 123, 53, 76, -87, 12]))
